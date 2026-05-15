9ae00287b579f62ab4df1bd919c2636c5e95d635
Edinson Madrid <edinsonmadrid@gmail.com>
2026-05-15 00:33:41 -0300

    chore: migrate to pnpm, add tab layout structure, and include time-based greeting hook

--- hooks/useGetHoursCurrent.ts
+++ hooks/useGetHoursCurrent.ts
@@ -1,20 +1,19 @@
 // import { useState, useEffect } from 'react';
 import dayjs from "dayjs";
 
-import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
-
+import utc from "dayjs/plugin/utc";
 
 export function useGetHoursCurrent() {
-    let saludo = ''; 
-    dayjs.extend(utc);
-    dayjs.extend(timezone);
-    // Para que todas las instancias de dayjs usen la zona horaria local por defecto
-    // Intenta poner tu zona horaria específica aquí para probar
-    const MI_ZONA = "America/Argentina/Buenos_Aires"; // o "America/Argentina/Buenos_Aires"
+	let saludo = "";
+	dayjs.extend(utc);
+	dayjs.extend(timezone);
+	// Para que todas las instancias de dayjs usen la zona horaria local por defecto
+	// Intenta poner tu zona horaria específica aquí para probar
+	const MI_ZONA = "America/Argentina/Buenos_Aires"; // o "America/Argentina/Buenos_Aires"
 
-    const horaActual = dayjs().tz(MI_ZONA).hour();
-    if (horaActual >= 6 && horaActual < 12) {
+	const horaActual = dayjs().tz(MI_ZONA).hour();
+	if (horaActual >= 6 && horaActual < 12) {
 		saludo = "Buenos Días";
 	} else if (horaActual >= 12 && horaActual < 19) {
 		saludo = "Buenas Tardes";
@@ -22,5 +21,5 @@ export function useGetHoursCurrent() {
 		saludo = "Buenas Noches";
 	}
 
-    return {horaActual, saludo};
+	return { horaActual, saludo };
 }
